3072f046115654f17ed0a81bb893f921196ef425
mohmohoo <mohmohoo85@gmail.com>
2020-02-09 16:34:11 +0000

    Add scenario 2 3 tests

--- SalesTax.Tests/Integration.cs
+++ SalesTax.Tests/Integration.cs
@@ -55,16 +55,58 @@ namespace SalesTax.Tests
 
             var basket = new Basket(book, musicCd, chocoletBar);
 
-            var reciept = target.ApplyTaxes(basket);
-            Assert.AreEqual("Sale Taxes: £1.50", reciept.SaleTaxesDescription);
-            Assert.AreEqual("Total: £29.83", reciept.TotalDescription);
+            var receipt = target.ApplyTaxes(basket);
+            Assert.AreEqual("Sale Taxes: £1.50", receipt.SaleTaxesDescription);
+            Assert.AreEqual("Total: £29.83", receipt.TotalDescription);
 
-            Assert.NotNull(reciept.ProductSummary);
-            Assert.AreEqual(3, reciept.ProductSummary.Length);
+            Assert.NotNull(receipt.ProductSummary);
+            Assert.AreEqual(3, receipt.ProductSummary.Length);
 
-            Assert.AreEqual("1 book: £12.49", reciept.ProductSummary[0]);
-            Assert.AreEqual("1 music CD: £16.49", reciept.ProductSummary[1]);
-            Assert.AreEqual("1 chocolate bar: £0.85", reciept.ProductSummary[2]);
+            Assert.AreEqual("1 book: £12.49", receipt.ProductSummary[0]);
+            Assert.AreEqual("1 music CD: £16.49", receipt.ProductSummary[1]);
+            Assert.AreEqual("1 chocolate bar: £0.85", receipt.ProductSummary[2]);
+        }
+
+        [Test]
+        public void Scenario2Test()
+        {
+            var chocolates = new Product(foodCategory, importedCategory) { Description = "imported box of chocolates", Price = 10.00f };
+            var perfume = new Product(otherCategory, importedCategory) { Description = "imported bottle of perfume", Price = 47.50f };
+
+            var basket = new Basket(chocolates, perfume);
+
+            var receipt = target.ApplyTaxes(basket);
+            Assert.AreEqual("Sale Taxes: £7.65", receipt.SaleTaxesDescription);
+            Assert.AreEqual("Total: £65.12", receipt.TotalDescription);
+
+            Assert.NotNull(receipt.ProductSummary);
+            Assert.AreEqual(2, receipt.ProductSummary.Length);
+
+            Assert.AreEqual("1 imported box of chocolates: £10.50", receipt.ProductSummary[0]);
+            Assert.AreEqual("1 imported bottle of perfume: £54.63", receipt.ProductSummary[1]);
+        }
+
+        [Test]
+        public void Scenario3Test()
+        {
+            var perfumeImported = new Product(otherCategory, importedCategory) { Description = "imported bottle of perfume", Price = 27.99f };
+            var perfume = new Product(otherCategory) { Description = "bottle of perfume", Price = 18.99f };
+            var pills = new Product(medicalCategory) { Description = "packet of headache pills", Price = 9.75f };
+            var chocolates = new Product(importedCategory) { Description = "box of imported chocolates", Price = 11.25f };
+
+            var basket = new Basket(perfumeImported, perfume, pills, chocolates);
+
+            var receipt = target.ApplyTaxes(basket);
+            Assert.AreEqual("Sale Taxes: £6.70", receipt.SaleTaxesDescription);
+            Assert.AreEqual("Total: £74.64", receipt.TotalDescription);
+
+            Assert.NotNull(receipt.ProductSummary);
+            Assert.AreEqual(4, receipt.ProductSummary.Length);
+
+            Assert.AreEqual("1 imported bottle of perfume: £32.19", receipt.ProductSummary[0]);
+            Assert.AreEqual("1 bottle of perfume: £20.89", receipt.ProductSummary[1]);
+            Assert.AreEqual("1 packet of headache pills: £9.75", receipt.ProductSummary[2]);
+            Assert.AreEqual("1 box of imported chocolates: £11.81", receipt.ProductSummary[3]);
         }
     }
 }
--- SalesTax/IProduct.cs
+++ SalesTax/IProduct.cs
@@ -104,27 +104,34 @@ namespace SalesTax
             var receipt = new Receipt();
             var totalWithTaxes = 0d;
 
-            foreach(var product in basket.Products)
+            foreach (var product in basket.Products)
             {
                 var taxes = ProductCategoryTaxLiabilities
-                    .SingleOrDefault(taxLiability =>
+                    .Where(taxLiability =>
                         product
                             .Categories
-                            .Any(category => taxLiability.Category.Description == category.Description))?.Taxes ?? new ITax[] { };
+                            .Any(category => taxLiability.Category.Description == category.Description))
+                    .SelectMany(liability => liability.Taxes) ?? new ITax[] { };
 
                 var totalTaxRate = 1 + taxes.Aggregate(0f, (soFar, currentTax) => soFar + currentTax.Rate);
-                totalWithTaxes += Math.Round(totalTaxRate * product.Price, 2);
+
+                totalWithTaxes += totalTaxRate * product.Price;
                 receipt.ProductSummary = receipt
                     .ProductSummary
-                    .Concat(new[] { $"1 {product.Description}: £{Math.Round(product.Price * totalTaxRate, 2).ToString("0.00")}" })
+                    .Concat(new[] { $"1 {product.Description}: £{(product.Price * totalTaxRate).ToString("0.00")}" })
                     .ToArray();
             }
-            
 
-            var taxesOnly = totalWithTaxes - basket.Products.Sum(product => product.Price);
-            receipt.SaleTaxesDescription = $"Sale Taxes: £{Math.Round(taxesOnly, 2).ToString("0.00")}";
-            receipt.TotalDescription = $"Total: £{totalWithTaxes}";
+            var taxesOnly = (float)totalWithTaxes - basket.Products.Sum(product => product.Price);
+            receipt.SaleTaxesDescription = $"Sale Taxes: £{Round(taxesOnly).ToString("0.00")}";
+            receipt.TotalDescription = $"Total: £{Math.Round(totalWithTaxes, 2).ToString("0.00")}";
             return receipt;
+        }
+
+        private static float Round(float value)
+        {
+            return (float)Math.Ceiling(value * 20) / 20;
+
         }
     }
 
