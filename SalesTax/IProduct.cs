0b02535bec456c8d31655db480a852df2f9e37b2
mohmohoo <mohmohoo85@gmail.com>
2020-02-09 09:37:26 +0000

    Add receipt class

--- SalesTax/IProduct.cs
+++ SalesTax/IProduct.cs
@@ -142,4 +142,19 @@ namespace SalesTax
 
         string TotalDescription { get; }
     }
+
+    public class Receipt
+        : IReceipt
+    {
+        public string[] ProductSummary { get; set; }
+
+        public string SaleTaxesDescription { get; set; }
+
+        public string TotalDescription { get; set; }
+
+        public Receipt()
+        {
+            ProductSummary = new string[] { };
+        }
+    }
 }
